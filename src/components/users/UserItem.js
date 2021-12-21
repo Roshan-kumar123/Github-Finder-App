a1ce74876bb44140b349b108dc198aef1b06d818
roshan-kumar123 <roshan31kumar31@gmail.com>
2021-12-22 00:16:52 +0530

    minor css fix

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -16,10 +16,7 @@ const UserItem = ({ user: { login, avatar_url } }) => {
 
         <div>
           <h2 className='card-title'>{login}</h2>
-          <Link
-            to={`/user/${login}`}
-            className='text-base-content text-opacity-40'
-          >
+          <Link to={`/user/${login}`} className='btn btn-xs glass'>
             Visit Profile
           </Link>
         </div>
